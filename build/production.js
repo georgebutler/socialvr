e587cfb5cdd51fd7ac3d4d12f06084ce7a1a7bbd
George Butler <georgebutleriv@gmail.com>
2022-03-11 10:23:06 -0800

    Test

--- build/production.js
+++ build/production.js
@@ -1,2 +1,2 @@
-var production=function(){"use strict";return()=>{console.log("if you have sourcemaps enabled in your devtools, click on main.js:5 --\x3e"),console.log("Update")}}();
+var production=function(){"use strict";console.log("\n\n\n\n\n\n\n\nPlease Work\n\n\n\n\n\n\n\n");return()=>{console.log("if you have sourcemaps enabled in your devtools, click on main.js:5 --\x3e"),console.log("Update")}}();
 //# sourceMappingURL=production.js.map
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import update from './update.js';
 
+console.log("\n\n\n\n\n\n\n\nPlease Work\n\n\n\n\n\n\n\n")
+
 export default () => {
     // even though Rollup is bundling all your files together, errors and
     // logs will still point to your original source modules
