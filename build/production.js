adcc4578f3cd9d0d4b8bf1b2a1f0e247dfee5e20
George Butler <georgebutleriv@gmail.com>
2022-03-11 10:31:02 -0800

    build

--- build/production.js
+++ build/production.js
@@ -1,2 +1,2 @@
-var production=function(){"use strict";console.log("\n\n\n\n\n\n\n\nPlease Work\n\n\n\n\n\n\n\n");return()=>{console.log("if you have sourcemaps enabled in your devtools, click on main.js:5 --\x3e"),console.log("Update")}}();
+!function(){"use strict";console.log("\n\n\n\n\n\n\n\nPlease Work\n\n\n\n\n\n\n\n"),console.log("\n\n\n\n\n\n\n\nUpdate\n\n\n\n\n\n\n\n")}();
 //# sourceMappingURL=production.js.map
