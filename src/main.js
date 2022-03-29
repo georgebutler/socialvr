37d80af22262d444c51cb71eb310e19323cda159
Caelen <caelenmensch@gmail.com>
2022-03-29 12:23:39 -0700

    sanity check

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
-import "./components/toolbox-button";
-import "./components/barge";
-import "./components/barge-button";
+// import "./components/toolbox-button";
+// import "./components/barge";
+// import "./components/barge-button";
 
-import "./systems/barge";
+// import "./systems/barge";
 
 const scene = document.querySelector("a-scene");
 
 console.log("[Social VR] Barge - Create Barge");
-const [barge, bargeToolboxButton] = CreateBarge();
-scene.appendChild(barge);
-scene.appendChild(bargeToolboxButton);
+// const [barge, bargeToolboxButton] = CreateBarge();
+// scene.appendChild(barge);
+// scene.appendChild(bargeToolboxButton);
 
-window.startPhaseTesting = function() {
-  let phase = 1;
-  barge.emit("advancePhaseEvent");
-  console.log(`[Social VR] Barge - Current Phase: ${phase}`);
-};
+// window.startPhaseTesting = function() {
+//   let phase = 1;
+//   barge.emit("advancePhaseEvent");
+//   console.log(`[Social VR] Barge - Current Phase: ${phase}`);
+// };
