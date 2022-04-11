a705374259060fcc09a771d1310f002a3d3901ae
Caelen <caelenmensch@gmail.com>
2022-04-11 13:37:17 -0700

    convo balance on production.js

--- src/main.js
+++ src/main.js
@@ -1,24 +1,3 @@
-// import "./components/toolbox-button";
-// import "./components/barge";
-// import "./components/barge-button";
-
-// import "./systems/barge";
-// import { CreateBarge } from "./systems/barge";
-
-// const scene = document.querySelector("a-scene");
-
-// console.log("[Social VR] Barge - Create Barge");
-// const [barge, bargeToolboxButton] = CreateBarge();
-// scene.appendChild(barge);
-// scene.appendChild(bargeToolboxButton);
-
-// window.startPhaseTesting = function() {
-//   let phase = 1;
-//   barge.emit("advancePhaseEvent");
-//   console.log(`[Social VR] Barge - Current Phase: ${phase}`);
-// };
-
-
 // from https://github.com/mkremins/hubs/blob/hubs-cloud-barge/src/max-additions.js
 
 /// utils
