22111c586691a6f220af94d5eb6270c597da39ac
George Butler <georgebutleriv@gmail.com>
2022-05-24 14:02:16 -0700

    Fixes

--- src/components/barge-clock.js
+++ src/components/barge-clock.js
@@ -4,10 +4,10 @@ AFRAME.registerComponent("socialvr-barge-clock", {
 
     this.text = document.createElement("a-entity");
     this.text.setAttribute("position", `0 0 0`);
-    this.text.setAttribute("text", `value: ; align: center; width: 4;`);
+    this.text.setAttribute("text", `value: ; align: center; side: double; width: 4;`);
     this.text.setAttribute("geometry", `primitive: plane; height: auto; width: 1;`);
     this.text.setAttribute("material", "color: #807e7e;");
-    this.text.setAttribute("animation", "property: rotation; to: 0 -360 0; easing: linear; loop: true; dur: 100000;")
+    this.text.setAttribute("animation", "property: rotation; to: 0 -360 0; easing: linear; loop: true; dur: 100000;");
     this.el.appendChild(this.text);
   },
 
