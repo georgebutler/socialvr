a518b65d6f0e1a7acc2f8860f003da70da74a985
George Butler <georgebutleriv@gmail.com>
2022-05-24 14:21:12 -0700

    Clock improvements

--- build/development.js
+++ build/development.js
@@ -716,7 +716,7 @@
 
       this.text = document.createElement("a-entity");
       this.text.setAttribute("position", `0 0 0`);
-      this.text.setAttribute("text", `value: ; align: center; width: 4;`);
+      this.text.setAttribute("text", `value: ; align: center; side: double; width: 4;`);
       this.text.setAttribute("geometry", `primitive: plane; height: auto; width: 1;`);
       this.text.setAttribute("material", "color: #807e7e;");
       this.text.setAttribute("animation", "property: rotation; to: 0 -360 0; easing: linear; loop: true; dur: 100000;");
