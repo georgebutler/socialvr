f69b0d613d29c4d01e7c62ae7516f87ebc50a357
George Butler <georgebutleriv@gmail.com>
2022-05-31 13:21:35 -0700

    dev

--- src/world-mover.js
+++ src/world-mover.js
@@ -7,10 +7,10 @@ function CreateWorldObject(data) {
         let position = new window.APP.utils.THREE.Vector3(transform.props.position.x, transform.props.position.y, transform.props.position.z);
         let rotation = new window.APP.utils.THREE.Euler(transform.props.rotation.x, transform.props.rotation.y, transform.props.rotation.z, "XYZ");
         let scale = new window.APP.utils.THREE.Vector3(transform.props.scale.x, transform.props.scale.y, transform.props.scale.z);
-    
+
         if (gltf) {
-            const { entity } = window.APP.utils.addMedia(gltf.props.src, "#static-media", 1, null, false, false, true, gltf.props, false);
-    
+            const { entity } = window.APP.utils.addMedia(gltf.props.src, "#static-media", 1, null, false, false, true, {}, false);
+
             entity.object3D.position.copy(position);
             entity.object3D.rotation.copy(rotation);
             entity.object3D.scale.copy(scale);
