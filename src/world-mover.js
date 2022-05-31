4e48bda4cc63044e799cc99fc4e53f6b9a998f4c
George Butler <georgebutleriv@gmail.com>
2022-05-31 13:05:35 -0700

    Fixes

--- src/world-mover.js
+++ src/world-mover.js
@@ -1,22 +1,25 @@
 function CreateWorldObject(data) {
     let transform = data.components.find(el => el.name === "transform");
-    let gltf = data.components.find(el => el.name === "gltf-model");
 
-    let position = new window.APP.utils.THREE.Vector3(transform.props.position.x, transform.props.position.y, transform.props.position.z);
-    let rotation = new window.APP.utils.THREE.Euler(transform.props.rotation.x, transform.props.rotation.y, transform.props.rotation.z, "XYZ");
-    let scale = new window.APP.utils.THREE.Vector3(transform.props.scale.x, transform.props.scale.y, transform.props.scale.z);
+    if (transform) {
+        let gltf = data.components.find(el => el.name === "gltf-model");
 
-    if (gltf) {
-        const { entity } = window.APP.utils.addMedia(gltf.props.src, "#static-media", 1, null, false, false, true, gltf.props, false);
-
-        entity.object3D.position.copy(position);
-        entity.object3D.rotation.copy(rotation);
-        entity.object3D.scale.copy(scale);
+        let position = new window.APP.utils.THREE.Vector3(transform.props.position.x, transform.props.position.y, transform.props.position.z);
+        let rotation = new window.APP.utils.THREE.Euler(transform.props.rotation.x, transform.props.rotation.y, transform.props.rotation.z, "XYZ");
+        let scale = new window.APP.utils.THREE.Vector3(transform.props.scale.x, transform.props.scale.y, transform.props.scale.z);
+    
+        if (gltf) {
+            const { entity } = window.APP.utils.addMedia(gltf.props.src, "#static-media", 1, null, false, false, true, gltf.props, false);
+    
+            entity.object3D.position.copy(position);
+            entity.object3D.rotation.copy(rotation);
+            entity.object3D.scale.copy(scale);
+        }
     }
 }
 
 export function InitWorldMover() {
-    fetch("https://statuesque-rugelach-4185bd.netlify.app/worlds/hangar.spoke")
+    fetch("https://statuesque-rugelach-4185bd.netlify.app/assets/hangar.spoke")
         .then(response => {
             return response.json();
         })
