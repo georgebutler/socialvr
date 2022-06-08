c28062c01711f8c384b280df2715cf7411513a38
George Butler <georgebutleriv@gmail.com>
2022-06-08 00:59:04 -0700

    Ranking

--- build/development.js
+++ build/development.js
@@ -227,6 +227,61 @@
     button.setAttribute("position", position);
     scene.appendChild(button);
 
+    // Frames
+    const frameKnowledge = document.querySelector(".knowledgeFrame_phase1");
+
+    for (let index = 1; index <= 3; index++) {
+      const slot = document.createElement("a-box");
+
+      slot.setAttribute("position", { x: 0, y: -1 + (0.4 * index), z: 0 });
+      slot.setAttribute("material", { color: "#FF0000", transparent: true, opacity: 0.5 });
+      slot.setAttribute("width", 2);
+      slot.setAttribute("height", 0.25);
+      slot.setAttribute("ksa-type", "knowledge");
+      slot.setAttribute("ksa-ranking", 4 - index);
+      frameKnowledge.appendChild(slot); 
+    }
+
+    const frameAbilities = document.querySelector(".KSA_ranking_frameglb_1_phase1");
+
+    for (let index = 1; index <= 3; index++) {
+      const slot = document.createElement("a-box");
+
+      slot.setAttribute("position", { x: 0, y: -1 + (0.4 * index), z: 0 });
+      slot.setAttribute("material", { color: "#FF0000", transparent: true, opacity: 0.5 });
+      slot.setAttribute("width", 2);
+      slot.setAttribute("height", 0.25);
+      slot.setAttribute("ksa-type", "abilities");
+      slot.setAttribute("ksa-ranking", 4 - index);
+      frameAbilities.appendChild(slot); 
+    }
+
+    const frameSkills = document.querySelector(".KSA_ranking_frameglb_phase1");
+
+    for (let index = 1; index <= 3; index++) {
+      const slot = document.createElement("a-box");
+
+      slot.setAttribute("position", { x: 0, y: -1 + (0.4 * index), z: 0 });
+      slot.setAttribute("material", { color: "#FF0000", transparent: true, opacity: 0.5 });
+      slot.setAttribute("width", 2);
+      slot.setAttribute("height", 0.25);
+      slot.setAttribute("ksa-type", "skills");
+      slot.setAttribute("ksa-ranking", 4 - index);
+      frameSkills.appendChild(slot); 
+    }
+
+    // Canidate Frame
+    const frameCanidate = document.querySelector(".candidate_frameglb_phase3");
+    const slot = document.createElement("a-box");
+
+    // slot.setAttribute("position", { x: 0, y: -1 + (0.4 * index), z: 0 });
+    slot.setAttribute("material", { color: "#FF0000", transparent: true, opacity: 0.5 });
+    slot.setAttribute("width", 0.5);
+    slot.setAttribute("height", 1);
+    slot.setAttribute("depth", 1);
+    slot.setAttribute("canidate-frame", "");
+    frameCanidate.appendChild(slot); 
+
     // World Mover
     const worldMover = document.createElement("a-entity");
     worldMover.setAttribute("socialvr-world-mover", "");
