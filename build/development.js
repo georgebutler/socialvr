0e6ac8d823f4706dcf4d9ce3bb287ab310ef82b4
George Butler <georgebutleriv@gmail.com>
2022-11-08 22:07:38 -0800

    environment

--- build/development.js
+++ build/development.js
@@ -194,13 +194,15 @@
     });
 
     APP.scene.addEventListener("environment-scene-loaded", () => {
-      const worldMover = document.createElement("a-entity");
+      const world = document.createElement("a-entity");
 
-      worldMover.setAttribute("socialvr-world-mover", {
+      world.setAttribute("socialvr-world-mover", {
         modelURL: "https://alex-leeds--statuesque-rugelach-4185bd.netlify.app/assets/environment-11.8.glb"
       });
 
-      window.APP.scene.appendChild(worldMover);
+      world.object3D.scale.set(0.1, 0.1, 0.1);
+      //world.object3D.scale.set(0.01, 0.01, 0.01);
+      window.APP.scene.appendChild(world);
     }, { once: true });
 
 })();
