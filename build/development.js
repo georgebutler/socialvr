83c3ced599897d2f0a62561abbafb7b880b7b0cb
George Butler <georgebutleriv@gmail.com>
2022-11-23 15:23:18 -0800

    300

--- build/development.js
+++ build/development.js
@@ -48,7 +48,7 @@
           });
           this.el.setAttribute("animation__worldshrink2", {
             property: "position",
-            to: "-200 0 0",
+            to: "-300 0 0",
             dur: 10000,
             startEvents: 'startworldshrink'
           });
