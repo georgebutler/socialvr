69ea72404300c1bbf080d38e3afb19fb61136934
George Butler <georgebutleriv@gmail.com>
2022-11-30 14:55:40 -0800

    Scaling test

--- src/components/speech.js
+++ src/components/speech.js
@@ -105,13 +105,15 @@ AFRAME.registerComponent("socialvr-speech", {
     for (const activeOrb of Object.values(this.activeSpeechOrbs)) {
       // grow each active speech orb by ORB_GROWTH_PER_TICK
       activeOrb.object3D.scale.add(new THREE.Vector3(0, ORB_GROWTH_PER_TICK * 7, 0));
-      activeOrb.matrixNeedsUpdate = true;
+      //activeOrb.matrixNeedsUpdate = true;
 
       // move its center upward by half of the growth amount,
       // to keep the bottom position fixed at the "now" plane
-      const pos = activeOrb.getAttribute("position");
-      //pos.y += ORB_GROWTH_PER_TICK * 0.8;
-      activeOrb.setAttribute("position", pos);
+      // const pos = activeOrb.getAttribute("position");
+      // pos.y += ORB_GROWTH_PER_TICK * 0.8;
+      // activeOrb.setAttribute("position", pos);
+      activeOrb.object3D.position.add(new THREE.Vector3(0, (ORB_GROWTH_PER_TICK * 7) / 2, 0));
+      activeOrb.object3D.matrixNeedsUpdate = true;
     }
   },
 
@@ -200,7 +202,7 @@ AFRAME.registerComponent("socialvr-speech", {
     const orb = document.createElement("a-entity");
     orb.classList.add("speechOrb");
     orb.setObject3D("mesh", new THREE.Mesh(geometry, material));
-    orb.getObject3D("mesh").applyMatrix4(new THREE.Matrix4().makeTranslation(0, size / 2, 0 ));
+    //orb.getObject3D("mesh").applyMatrix4(new THREE.Matrix4().makeTranslation(0, size / 2, 0 ));
 
     // add the orb to the scene
     this.el.appendChild(orb);
